d0ae068c61572fe1af334a93d6a2ef0455b812c1
doodledood <aviram.kofman@gmail.com>
2024-01-20 18:26:42 +0200

    update bshr stuff

--- chatflock/use_cases/bshr.py
+++ chatflock/use_cases/bshr.py
@@ -204,8 +204,8 @@ def generate_queries(
         chat_messages=chat.get_messages(), chat_model=chat_model, output_schema=QueryGenerationResult
     )
 
-    # if state.information_need:
-    state.information_need = output.information_need
+    if state.information_need:
+        state.information_need = output.information_need
 
     if state.queries_to_run is None:
         state.queries_to_run = []
